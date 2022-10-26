0f66bdf2a86309a1b6d5903e63b8cb0cb003664a
CoinerLo <coiner4677@gmail.com>
2022-10-26 19:22:00 +0300

    YA-15  Loaded react-hook-form and made the basis of the page.

--- packages/client/src/pages/SignUp/index.tsx
+++ packages/client/src/pages/SignUp/index.tsx
@@ -1,3 +1,123 @@
+import { useForm } from 'react-hook-form'
+
 export const SignUp = () => {
-  return <div>This is leaderboard!</div>
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onBlur',
+  })
+  const onSubmit = (data: Record<string, string>) => {
+    console.log(data)
+    reset()
+  }
+
+  return (
+    <div>
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Ваше имя:
+          <input
+            {...register('firstName', {
+              required: 'Поле обязательно к заполнению!',
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+            })}></input>
+        </label>
+        <div>
+          {errors?.firstName && (
+            <p>
+              {String(errors?.firstName?.message) || 'Поле заполнено не верно!'}
+            </p>
+          )}
+        </div>
+
+        <label>
+          NickName:
+          <input
+            {...register('nickName', {
+              required: 'Поле обязательно к заполнению!',
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+            })}></input>
+        </label>
+        <div>
+          {errors?.nickName && (
+            <p>
+              {String(errors?.nickName?.message) || 'Поле заполнено не верно!'}
+            </p>
+          )}
+        </div>
+
+        <label>
+          Email:
+          <input
+            {...register('email', {
+              required: 'Поле обязательно к заполнению!',
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+            })}></input>
+        </label>
+        <div>
+          {errors?.email && (
+            <p>
+              {String(errors?.email?.message) || 'Поле заполнено не верно!'}
+            </p>
+          )}
+        </div>
+
+        <label>
+          Пароль:
+          <input
+            type={'password'}
+            {...register('password', {
+              required: 'Поле обязательно к заполнению!',
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+            })}></input>
+        </label>
+        <div>
+          {errors?.password && (
+            <p>
+              {String(errors?.password?.message) || 'Поле заполнено не верно!'}
+            </p>
+          )}
+        </div>
+
+        <label>
+          Повтор пароля:
+          <input
+            type="password"
+            {...register('passwordRepeat', {
+              required: 'Поле обязательно к заполнению!',
+              minLength: {
+                value: 4,
+                message: 'Минимум 4 символа',
+              },
+            })}></input>
+        </label>
+        <div>
+          {errors?.passwordRepeat && (
+            <p>
+              {String(errors?.passwordRepeat?.message) ||
+                'Поле заполнено не верно!'}
+            </p>
+          )}
+        </div>
+
+        <input type={'submit'} disabled={!isValid}></input>
+      </form>
+    </div>
+  )
 }
