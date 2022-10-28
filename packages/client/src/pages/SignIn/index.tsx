c45ab0c056404ef94f67c4ba951f7a1030f4d07a
ATMR90 <Art1603@yandex.ru>
2022-10-28 19:53:15 +0300

    YA-14 установлен sass, изменена страница авторизации v0.2, добавлен файл со стилями

--- packages/client/src/pages/SignIn/index.tsx
+++ packages/client/src/pages/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
+import './signIn.scss' 
 
 export interface ISignInData {
   login: string
@@ -6,6 +7,9 @@ export interface ISignInData {
 }
 
 export const SignIn = () => {
+
+  const title = 'Вход'
+
   const {
     register,
     handleSubmit,
@@ -19,37 +23,53 @@ export const SignIn = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('login', {
-            required: 'Это обязательное поле',
-            pattern: {
-              value: /^(?=.*[a-z])[a-zA-Z0-9_-]{3,20}$/,
-              message:
-                'Логин должен содержать от 3 до 20 символов, латиница, может содержать цифры, но не состоять из них, без пробелов, без спецсимволов (допустимы дефис и нижнее подчеркивание',
-            },
-          })}
-          type="text"
-          placeholder="Логин"
-        />
-        {errors?.login && (
-          <div style={{ color: 'red' }}>{errors.login.message}</div>
-        )}
-        <input
-          {...register('password', {
-            required: 'Это обязательное поле',
-          })}
-          type="password"
-          placeholder="Пароль"
-        />
-        {errors?.password && (
-          <div style={{ color: 'red' }}>{errors.password.message}</div>
-        )}
-        <div>
-          <button>Войти</button>
+      <form className='ya-form' onSubmit={handleSubmit(onSubmit)}>
+        <div className='ya-form__container ya-form__container_shadow'>
+          <div className="ya-form__title">
+            {title}
+          </div>
+          <div className="ya-form__fields">
+            <div className=" ya-form__field ya-field">
+              <input 
+                className='ya-field__input'
+                id='idInput'
+                {...register('login', {
+                  required: 'Это обязательное поле',
+                  pattern: {
+                    value: /^(?=.*[a-z])[a-zA-Z0-9_-]{3,20}$/,
+                    message:
+                      'Логин должен содержать от 3 до 20 символов, латиница, может содержать цифры, но не состоять из них, без пробелов, без спецсимволов (допустимы дефис и нижнее подчеркивание',
+                  },
+                })}
+                type="text"
+              />
+              <label className='ya-field__label' htmlFor='idInput'>
+                Логин
+              </label>
+              {errors?.login && (
+                <div className='ya-field__error'>{errors.login.message}</div>
+              )}
+            </div>
+            <div className="ya-field ya-form__field">
+              <input className='ya-field__input'
+                {...register('password', {
+                  required: 'Это обязательное поле',
+                })}
+                type="password"
+              />
+              <label className='ya-field__label' htmlFor='idInput'>
+                Пароль
+              </label>
+              {errors?.password && (
+                <div className='ya-field__error'>{errors.password.message}</div>
+              )}
+            </div>
+          </div>
+          <div className='ya-form__footer'>
+            <button className='ya-btn ya-btn_main ya-form__btn'>Войти</button>
+            <button className='ya-btn ya-form__btn'>Регистрация</button>
+          </div>
         </div>
       </form>
-    </div>
   )
 }
