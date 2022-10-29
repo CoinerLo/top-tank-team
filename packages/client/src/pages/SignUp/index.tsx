85cf431c45e1aa344346c7f2e0b21209b632b0a7
Xroniks <Xroniks25@gmail.com>
2022-10-30 00:15:10 +0300

    YA-15 Finished the validation. Completed the registration form.

--- packages/client/src/pages/SignUp/index.tsx
+++ packages/client/src/pages/SignUp/index.tsx
@@ -5,11 +5,16 @@ import {
   useFormState,
 } from 'react-hook-form'
 import { Typography, Button, Container, TextField } from '@mui/material'
-
-interface ISingUpForm {
-  login: string
-  password: string
-}
+import { NavLink } from 'react-router-dom'
+import {
+  loginValidation,
+  passwordValidation,
+  emailValidation,
+  firstNameValidation,
+  secondNameValidation,
+  phoneValidation,
+} from '../../utils/validation'
+import { ISingUpForm } from '../../typings'
 
 export const SignUp = () => {
   const { handleSubmit, reset, control } = useForm<ISingUpForm>({
@@ -20,8 +25,18 @@ export const SignUp = () => {
     control,
   })
   const onSubmit: SubmitHandler<ISingUpForm> = data => {
+    //Для проверки (знать что выводится, потом сделаю логику)
     console.log(data)
-    reset({ login: '', password: '' })
+
+    //Понимаю, что здесь это выглядит лишним, но не знаю как отдельно и куда.
+    reset({
+      first_name: '',
+      second_name: '',
+      login: '',
+      email: '',
+      password: '',
+      phone: '',
+    })
   }
 
   return (
@@ -30,145 +45,139 @@ export const SignUp = () => {
         <Typography variant="h4" align="center">
           Регистрация
         </Typography>
+
         <Controller
           control={control}
-          rules={{
-            required: 'Обязательное поле для ввода',
-            minLength: { value: 5, message: 'Должно быть больше 5 символов' },
-          }}
+          rules={firstNameValidation}
+          name="first_name"
+          render={({ field }) => (
+            <TextField
+              label="Имя"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
+              onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
+              value={field.value}
+              error={!!errors.first_name?.message}
+              helperText={errors?.first_name?.message}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          rules={secondNameValidation}
+          name="second_name"
+          render={({ field }) => (
+            <TextField
+              label="Фамилия"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
+              onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
+              value={field.value}
+              error={!!errors.second_name?.message}
+              helperText={errors?.second_name?.message}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          rules={loginValidation}
           name="login"
-          render={({ field: { value, onChange, onBlur } }) => (
+          render={({ field }) => (
             <TextField
               label="Логин"
               size="small"
               fullWidth={true}
               margin="normal"
               className="auth-form__input"
-              onChange={e => onChange(e)}
-              onBlur={() => onBlur()}
-              value={value}
+              onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
+              value={field.value}
               error={!!errors.login?.message}
               helperText={errors?.login?.message}
             />
           )}
         />
+
         <Controller
           control={control}
-          rules={{ required: true }}
+          rules={emailValidation}
+          name="email"
+          render={({ field }) => (
+            <TextField
+              label="Email"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
+              onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
+              value={field.value}
+              error={!!errors.email?.message}
+              helperText={errors?.email?.message}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          rules={passwordValidation}
           name="password"
           render={({ field }) => (
             <TextField
               label="Пароль"
-              size="small"
               type="password"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
               onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
               value={field.value}
+              error={!!errors.password?.message}
+              helperText={errors?.password?.message}
             />
           )}
         />
-        <Button type="submit" variant={'contained'}>
+
+        <Controller
+          control={control}
+          rules={phoneValidation}
+          name="phone"
+          render={({ field }) => (
+            <TextField
+              label="Телефон"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
+              onChange={e => field.onChange(e)}
+              onBlur={() => field.onBlur()}
+              value={field.value}
+              error={!!errors.phone?.message}
+              helperText={errors?.phone?.message}
+            />
+          )}
+        />
+
+        <Button
+          type="submit"
+          variant={'contained'}
+          disabled={!isValid}
+          sx={{ mt: '10px' }}>
           Зарегистрироваться
         </Button>
 
-        {/*<label>*/}
-        {/*  Ваше имя:*/}
-        {/*  <input*/}
-        {/*    {...register('firstName', {*/}
-        {/*      required: 'Поле обязательно к заполнению!',*/}
-        {/*      minLength: {*/}
-        {/*        value: 4,*/}
-        {/*        message: 'Минимум 4 символа',*/}
-        {/*      },*/}
-        {/*    })}></input>*/}
-        {/*</label>*/}
-        {/*<div>*/}
-        {/*  {errors?.firstName && (*/}
-        {/*    <p>*/}
-        {/*      {String(errors?.firstName?.message) || 'Поле заполнено не верно!'}*/}
-        {/*    </p>*/}
-        {/*  )}*/}
-        {/*</div>*/}
-
-        {/*<label>*/}
-        {/*  NickName:*/}
-        {/*  <input*/}
-        {/*    {...register('nickName', {*/}
-        {/*      required: 'Поле обязательно к заполнению!',*/}
-        {/*      minLength: {*/}
-        {/*        value: 4,*/}
-        {/*        message: 'Минимум 4 символа',*/}
-        {/*      },*/}
-        {/*    })}></input>*/}
-        {/*</label>*/}
-        {/*<div>*/}
-        {/*  {errors?.nickName && (*/}
-        {/*    <p>*/}
-        {/*      {String(errors?.nickName?.message) || 'Поле заполнено не верно!'}*/}
-        {/*    </p>*/}
-        {/*  )}*/}
-        {/*</div>*/}
-
-        {/*<label>*/}
-        {/*  Email:*/}
-        {/*  <input*/}
-        {/*    {...register('email', {*/}
-        {/*      required: 'Поле обязательно к заполнению!',*/}
-        {/*      minLength: {*/}
-        {/*        value: 4,*/}
-        {/*        message: 'Минимум 4 символа',*/}
-        {/*      },*/}
-        {/*    })}></input>*/}
-        {/*</label>*/}
-        {/*<div>*/}
-        {/*  {errors?.email && (*/}
-        {/*    <p>*/}
-        {/*      {String(errors?.email?.message) || 'Поле заполнено не верно!'}*/}
-        {/*    </p>*/}
-        {/*  )}*/}
-        {/*</div>*/}
-
-        {/*<label>*/}
-        {/*  Пароль:*/}
-        {/*  <input*/}
-        {/*    type={'password'}*/}
-        {/*    {...register('password', {*/}
-        {/*      required: 'Поле обязательно к заполнению!',*/}
-        {/*      minLength: {*/}
-        {/*        value: 4,*/}
-        {/*        message: 'Минимум 4 символа',*/}
-        {/*      },*/}
-        {/*    })}></input>*/}
-        {/*</label>*/}
-        {/*<div>*/}
-        {/*  {errors?.password && (*/}
-        {/*    <p>*/}
-        {/*      {String(errors?.password?.message) || 'Поле заполнено не верно!'}*/}
-        {/*    </p>*/}
-        {/*  )}*/}
-        {/*</div>*/}
-
-        {/*<label>*/}
-        {/*  Повтор пароля:*/}
-        {/*  <input*/}
-        {/*    type="password"*/}
-        {/*    {...register('passwordRepeat', {*/}
-        {/*      required: 'Поле обязательно к заполнению!',*/}
-        {/*      minLength: {*/}
-        {/*        value: 4,*/}
-        {/*        message: 'Минимум 4 символа',*/}
-        {/*      },*/}
-        {/*    })}></input>*/}
-        {/*</label>*/}
-        {/*<div>*/}
-        {/*  {errors?.passwordRepeat && (*/}
-        {/*    <p>*/}
-        {/*      {String(errors?.passwordRepeat?.message) ||*/}
-        {/*        'Поле заполнено не верно!'}*/}
-        {/*    </p>*/}
-        {/*  )}*/}
-        {/*</div>*/}
-
-        {/*<input type={'submit'} disabled={!isValid}></input>*/}
+        <Typography variant="subtitle1" component="span" sx={{ ml: '10px' }}>
+          <NavLink to="/signin">Войти</NavLink>
+        </Typography>
       </form>
     </Container>
   )
