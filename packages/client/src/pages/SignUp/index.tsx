8ea7dac8fad0e5085f2b184e8d8ea5cc9ce966f0
Xroniks <Xroniks25@gmail.com>
2022-10-29 22:04:09 +0300

    YA-15 Intermediate commit. Loaded functionality from the main branch

--- packages/client/src/pages/SignUp/index.tsx
+++ packages/client/src/pages/SignUp/index.tsx
@@ -1,123 +1,175 @@
-import { useForm } from 'react-hook-form'
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+  useFormState,
+} from 'react-hook-form'
+import { Typography, Button, Container, TextField } from '@mui/material'
+
+interface ISingUpForm {
+  login: string
+  password: string
+}
 
 export const SignUp = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors, isValid },
-  } = useForm({
+  const { handleSubmit, reset, control } = useForm<ISingUpForm>({
     mode: 'onBlur',
+    reValidateMode: 'onChange',
   })
-  const onSubmit = (data: Record<string, string>) => {
+  const { errors, isValid } = useFormState({
+    control,
+  })
+  const onSubmit: SubmitHandler<ISingUpForm> = data => {
     console.log(data)
-    reset()
+    reset({ login: '', password: '' })
   }
 
   return (
-    <div>
-      <h1>Регистрация</h1>
+    <Container maxWidth="sm">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Ваше имя:
-          <input
-            {...register('firstName', {
-              required: 'Поле обязательно к заполнению!',
-              minLength: {
-                value: 4,
-                message: 'Минимум 4 символа',
-              },
-            })}></input>
-        </label>
-        <div>
-          {errors?.firstName && (
-            <p>
-              {String(errors?.firstName?.message) || 'Поле заполнено не верно!'}
-            </p>
+        <Typography variant="h4" align="center">
+          Регистрация
+        </Typography>
+        <Controller
+          control={control}
+          rules={{
+            required: 'Обязательное поле для ввода',
+            minLength: { value: 5, message: 'Должно быть больше 5 символов' },
+          }}
+          name="login"
+          render={({ field: { value, onChange, onBlur } }) => (
+            <TextField
+              label="Логин"
+              size="small"
+              fullWidth={true}
+              margin="normal"
+              className="auth-form__input"
+              onChange={e => onChange(e)}
+              onBlur={() => onBlur()}
+              value={value}
+              error={!!errors.login?.message}
+              helperText={errors?.login?.message}
+            />
           )}
-        </div>
-
-        <label>
-          NickName:
-          <input
-            {...register('nickName', {
-              required: 'Поле обязательно к заполнению!',
-              minLength: {
-                value: 4,
-                message: 'Минимум 4 символа',
-              },
-            })}></input>
-        </label>
-        <div>
-          {errors?.nickName && (
-            <p>
-              {String(errors?.nickName?.message) || 'Поле заполнено не верно!'}
-            </p>
+        />
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          name="password"
+          render={({ field }) => (
+            <TextField
+              label="Пароль"
+              size="small"
+              type="password"
+              onChange={e => field.onChange(e)}
+              value={field.value}
+            />
           )}
-        </div>
+        />
+        <Button type="submit" variant={'contained'}>
+          Зарегистрироваться
+        </Button>
 
-        <label>
-          Email:
-          <input
-            {...register('email', {
-              required: 'Поле обязательно к заполнению!',
-              minLength: {
-                value: 4,
-                message: 'Минимум 4 символа',
-              },
-            })}></input>
-        </label>
-        <div>
-          {errors?.email && (
-            <p>
-              {String(errors?.email?.message) || 'Поле заполнено не верно!'}
-            </p>
-          )}
-        </div>
+        {/*<label>*/}
+        {/*  Ваше имя:*/}
+        {/*  <input*/}
+        {/*    {...register('firstName', {*/}
+        {/*      required: 'Поле обязательно к заполнению!',*/}
+        {/*      minLength: {*/}
+        {/*        value: 4,*/}
+        {/*        message: 'Минимум 4 символа',*/}
+        {/*      },*/}
+        {/*    })}></input>*/}
+        {/*</label>*/}
+        {/*<div>*/}
+        {/*  {errors?.firstName && (*/}
+        {/*    <p>*/}
+        {/*      {String(errors?.firstName?.message) || 'Поле заполнено не верно!'}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
 
-        <label>
-          Пароль:
-          <input
-            type={'password'}
-            {...register('password', {
-              required: 'Поле обязательно к заполнению!',
-              minLength: {
-                value: 4,
-                message: 'Минимум 4 символа',
-              },
-            })}></input>
-        </label>
-        <div>
-          {errors?.password && (
-            <p>
-              {String(errors?.password?.message) || 'Поле заполнено не верно!'}
-            </p>
-          )}
-        </div>
+        {/*<label>*/}
+        {/*  NickName:*/}
+        {/*  <input*/}
+        {/*    {...register('nickName', {*/}
+        {/*      required: 'Поле обязательно к заполнению!',*/}
+        {/*      minLength: {*/}
+        {/*        value: 4,*/}
+        {/*        message: 'Минимум 4 символа',*/}
+        {/*      },*/}
+        {/*    })}></input>*/}
+        {/*</label>*/}
+        {/*<div>*/}
+        {/*  {errors?.nickName && (*/}
+        {/*    <p>*/}
+        {/*      {String(errors?.nickName?.message) || 'Поле заполнено не верно!'}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
 
-        <label>
-          Повтор пароля:
-          <input
-            type="password"
-            {...register('passwordRepeat', {
-              required: 'Поле обязательно к заполнению!',
-              minLength: {
-                value: 4,
-                message: 'Минимум 4 символа',
-              },
-            })}></input>
-        </label>
-        <div>
-          {errors?.passwordRepeat && (
-            <p>
-              {String(errors?.passwordRepeat?.message) ||
-                'Поле заполнено не верно!'}
-            </p>
-          )}
-        </div>
+        {/*<label>*/}
+        {/*  Email:*/}
+        {/*  <input*/}
+        {/*    {...register('email', {*/}
+        {/*      required: 'Поле обязательно к заполнению!',*/}
+        {/*      minLength: {*/}
+        {/*        value: 4,*/}
+        {/*        message: 'Минимум 4 символа',*/}
+        {/*      },*/}
+        {/*    })}></input>*/}
+        {/*</label>*/}
+        {/*<div>*/}
+        {/*  {errors?.email && (*/}
+        {/*    <p>*/}
+        {/*      {String(errors?.email?.message) || 'Поле заполнено не верно!'}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
 
-        <input type={'submit'} disabled={!isValid}></input>
+        {/*<label>*/}
+        {/*  Пароль:*/}
+        {/*  <input*/}
+        {/*    type={'password'}*/}
+        {/*    {...register('password', {*/}
+        {/*      required: 'Поле обязательно к заполнению!',*/}
+        {/*      minLength: {*/}
+        {/*        value: 4,*/}
+        {/*        message: 'Минимум 4 символа',*/}
+        {/*      },*/}
+        {/*    })}></input>*/}
+        {/*</label>*/}
+        {/*<div>*/}
+        {/*  {errors?.password && (*/}
+        {/*    <p>*/}
+        {/*      {String(errors?.password?.message) || 'Поле заполнено не верно!'}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
+
+        {/*<label>*/}
+        {/*  Повтор пароля:*/}
+        {/*  <input*/}
+        {/*    type="password"*/}
+        {/*    {...register('passwordRepeat', {*/}
+        {/*      required: 'Поле обязательно к заполнению!',*/}
+        {/*      minLength: {*/}
+        {/*        value: 4,*/}
+        {/*        message: 'Минимум 4 символа',*/}
+        {/*      },*/}
+        {/*    })}></input>*/}
+        {/*</label>*/}
+        {/*<div>*/}
+        {/*  {errors?.passwordRepeat && (*/}
+        {/*    <p>*/}
+        {/*      {String(errors?.passwordRepeat?.message) ||*/}
+        {/*        'Поле заполнено не верно!'}*/}
+        {/*    </p>*/}
+        {/*  )}*/}
+        {/*</div>*/}
+
+        {/*<input type={'submit'} disabled={!isValid}></input>*/}
       </form>
-    </div>
+    </Container>
   )
 }
