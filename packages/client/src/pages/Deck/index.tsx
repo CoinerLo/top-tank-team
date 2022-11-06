e252d9d937a15aadddd3395d1f1e66b7430c73e8
ATMR90 <Art1603@yandex.ru>
2022-11-06 19:42:47 +0300

    YA-27 добавлен класс ошибок ErrorBoundary, обернут App, тестовая ошибка в Deck

--- packages/client/src/pages/Deck/index.tsx
+++ packages/client/src/pages/Deck/index.tsx
@@ -1,5 +1,13 @@
 import { Container } from '@mui/material'
 
 export const Deck = () => {
-  return <Container disableGutters>This is Deck!</Container>
+  let obj = {
+    name: 'user',
+    id: 15,
+  }
+  // eslint-disable-next-line
+  //@ts-ignore
+  obj = null
+
+  return <Container disableGutters>This is Deck!`${obj.name}`</Container>
 }
