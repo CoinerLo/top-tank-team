94f3e9d269e8d75ef3565f788de247b3287243d9
ATMR90 <Art1603@yandex.ru>
2022-11-07 00:08:29 +0300

    YA-27 убрал ошибку из Deck

--- packages/client/src/pages/Deck/index.tsx
+++ packages/client/src/pages/Deck/index.tsx
@@ -1,13 +1,5 @@
 import { Container } from '@mui/material'
 
 export const Deck = () => {
-  let obj = {
-    name: 'user',
-    id: 15,
-  }
-  // eslint-disable-next-line
-  //@ts-ignore
-  obj = null
-
-  return <Container disableGutters>This is Deck!`${obj.name}`</Container>
+  return <Container disableGutters>This is Deck!</Container>
 }
