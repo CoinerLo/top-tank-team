8877e82b12da14de5afd3a36edd0efeb10a0200e
Никита <kondor12371@yandex.ru>
2022-11-12 15:37:40 +0300

    removed no-unused-vars

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import browserHistory from './browser-history'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
