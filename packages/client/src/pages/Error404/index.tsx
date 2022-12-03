6c7ad9c997736a01fcc86a0da86942cebd3fa747
Xroniks <Xroniks25@gmail.com>
2022-12-03 14:32:51 +0300

    YA-69 decorated the page 404

--- packages/client/src/pages/Error404/index.tsx
+++ packages/client/src/pages/Error404/index.tsx
@@ -1,3 +1,61 @@
+import { Button, Box, Typography, Container, Link } from '@mui/material'
+import React from 'react'
+import { AppRoute } from '../../utils/consts'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '100px',
+  },
+  mainBtn: {
+    marginX: '40px',
+    padding: '20px 80px',
+    fontSize: '2rem',
+  },
+}
+
 export const Error404 = () => {
-  return <div>This is Error404!</div>
+  const navigate = useNavigate()
+
+  const headquarters = () => {
+    navigate(`/${AppRoute.Headquarters}`)
+  }
+
+  return (
+    <Container disableGutters sx={styles.container}>
+      <Box
+        component="img"
+        src="/Erorr404.png"
+        alt="HomeLogo"
+        sx={{
+          height: '350px',
+          borderBottom: '4px solid rgba(234,227,204,20%)',
+        }}
+      />
+      <Typography variant="h3" component="span" sx={{ marginTop: '10px' }}>
+        Ваш отряд заблудился!
+      </Typography>
+      <Box
+        sx={{
+          marginBottom: '5px',
+
+          borderRadius: '5px',
+          padding: '20px',
+        }}>
+        <Typography variant="body1">
+          Здесь Вы точно ничего не найдёте, прошу вернуться в свой штаб!
+        </Typography>
+      </Box>
+      <Button onClick={headquarters} variant="secondary" sx={styles.mainBtn}>
+        Отыскать своих
+      </Button>
+      <Typography
+        variant="subtitle1"
+        component="span"
+        sx={{ marginTop: '20px' }}></Typography>
+    </Container>
+  )
 }
