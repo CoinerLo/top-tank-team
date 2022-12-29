1fc4e2202f1a0e75fc3195022cd04a3cb966a595
Kamil Khismatullin <kami16ru@gmail.com>
2022-12-29 18:24:27 +0300

    YA-25: fix comments

--- packages/client/src/components/leaderboard/table/BasicTable.test.tsx
+++ packages/client/src/components/leaderboard/table/BasicTable.test.tsx
@@ -2,17 +2,9 @@ import { BasicTable } from './BasicTable'
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from '../../../store'
-// import { useAppselector } from '../../../hooks'
 
-// import * as redux from 'react-redux'
-
-// const spy = jest.spyOn(redux, 'useAppselector')
-
-describe('Basic table component. NO TEST!', () => {
-  it('Renders table. NO TEST!', () => {
-    // jest.mock('useAppselector')
-    jest.mock('../../../hooks')
-
+describe('Basic table component.', () => {
+  it('Renders table.', () => {
     render(
       <Provider store={store}>
         <BasicTable />
