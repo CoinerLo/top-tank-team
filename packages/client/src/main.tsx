20a3f148497c0492990d335a6321f0e5ef02db4d
ATMR90 <Art1603@yandex.ru>
2023-01-25 01:36:39 +0300

    YA-64 Service Workers

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -22,16 +22,21 @@ ReactDOM.hydrateRoot(
 )
 
 function startServiceWorker() {
-  if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-          navigator.serviceWorker.register("./sw.js").then(registration => {
-              console.log("ServiceWorker registration successful with scope: ", registration.scope);
-          }).catch((error: string) => {
-              console.log("ServiceWorker registration failed: ", error);
-          });
-      });
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('./sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error)
+        })
+    })
   }
-
 }
 
-startServiceWorker(); 
+startServiceWorker()
