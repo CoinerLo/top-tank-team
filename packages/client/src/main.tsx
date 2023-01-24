9f11e197440c196e3df3998ca77d11780c9823ee
ATMR90 <Art1603@yandex.ru>
2023-01-25 01:34:15 +0300

    YA-64 Service Workers

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -20,3 +20,18 @@ ReactDOM.hydrateRoot(
     </HistoryRouter>
   </ErrorBoundary>
 )
+
+function startServiceWorker() {
+  if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+          navigator.serviceWorker.register("./sw.js").then(registration => {
+              console.log("ServiceWorker registration successful with scope: ", registration.scope);
+          }).catch((error: string) => {
+              console.log("ServiceWorker registration failed: ", error);
+          });
+      });
+  }
+
+}
+
+startServiceWorker(); 
